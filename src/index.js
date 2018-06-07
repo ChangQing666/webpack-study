8bab7a29ed52d7ae435a2d4e289e2959fb9abf71
chang.xianglin <chang.xianglin@jyall.com>
2018-06-07 11:06:20 +0800

    代码分离splitChunks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
-// import './styles.css'
+import {cube} from './math.js';
+if (process.env.NODE_ENV!=='production'){
+    console.log('Looks like we are in development mode!')
+}
+console.log('当前环境：', process.env.NODE_ENV);
 function component () {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe;
-    element.appendChild(btn);
-
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'hello webpack',
+        '5 cubed is equal to'+ cube(5)
+    ].join('\n\n');
     return element;
 }
 document.body.appendChild(component());
--- webpack.common.js
+++ webpack.common.js
@@ -4,16 +4,22 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
+        index: './src/index.js',
+        another: './src/another-module.js'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Production'
-        }),
+            title: '代码分离'
+        })
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        splitChunks:{
+            chunks: 'all'
+        }
     }
 }
