c08eca5abccba39edc078a52e718111ca35b6136
chang.xianglin <chang.xianglin@jyall.com>
2018-06-07 10:03:57 +0800

    hmr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css'
 function component () {
     var element = document.createElement('div');
     var btn = document.createElement('button');
@@ -13,10 +14,14 @@ function component () {
     return element;
 }
 document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
-if(module.hot){
-    module.hot.accept('./print.js',function(){
+if (module.hot) {
+    module.hot.accept('./print.js', function(){
         console.log('Accepting the updated printMe module!');
-        printMe();
+        document.body.removeChild(element);
+        element = component();
+        document.dody.appendChild(element);
     })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+
 module.exports = {
     entry: {
         app: './src/index.js',
@@ -10,6 +11,14 @@ module.exports = {
     devServer: {
       contentBase: './dist',
       hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
